5ba45245d912c946d29355f91f8d815050255c2f
SIST <SIST@DESKTOP-AD8C9P3>
2020-07-08 17:08:08 +0900

    깃허브

--- 20200619-깃허브/src/MainClass.java
+++ 20200619-깃허브/src/MainClass.java
@@ -6,6 +6,13 @@ public class MainClass {
 		System.out.println("Hello Java!!");
 		System.out.println("Hello Java!!");
 		System.out.println("Hello Java!!");
+		
+		int x=2;
+		int y=10;
+		int z=0;
+		
+		z=x++*2+--y-5+x*(y%2);
+		System.out.println(z);
 
 	}
 
