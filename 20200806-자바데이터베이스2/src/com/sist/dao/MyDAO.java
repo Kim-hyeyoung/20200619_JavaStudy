ee5a1e8c98d36961d43624cf37cff8e6d54f17ca
SIST <SIST@DESKTOP-AD8C9P3>
2020-08-28 14:55:18 +0900

    자바데이터베이스2

---
+++ 20200806-자바데이터베이스2/src/com/sist/dao/MyDAO.java
@@ -0,0 +1,15 @@
+package com.sist.dao;
+import java.util.*;
+import java.sql.*;
+public class MyDAO {
+	EmpDAO dao;
+	public MyDAO()
+	{
+		dao=EmpDAO.newInstance();
+	}
+	public void empAllData()
+	{
+		dao.getConnection();
+	}
+
+}
